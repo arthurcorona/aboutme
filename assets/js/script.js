a409dc2058c4dbdd0fea29130232473fcec74415
Arthur Corona <coronaggp@gmail.com>
2022-08-05 21:55:50 -0300

    svg

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,19 @@ if(navToggle) {
         navMenu.classList.add('show-menu')
     })
 }
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+        // MENU MOBILE (remove)
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // quando clicar em nav_link, será removido a class "show-menu"
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
